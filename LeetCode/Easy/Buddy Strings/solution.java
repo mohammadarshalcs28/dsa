72cb939dc5025db828320c0c8c96692120547794
mohammadarshalcs28 <mohamma.arshal.cs28@iilm.edu>
2026-08-26 10:28:47 +0530

    Solved: Buddy Strings [Easy]

--- LeetCode/Easy/Buddy Strings/solution.java
+++ LeetCode/Easy/Buddy Strings/solution.java
@@ -1,41 +1,28 @@
 class Solution {
+
     public boolean buddyStrings(String s, String goal) {
 
         int n = s.length();
         int k = goal.length();
 
-        // Length must be same
+        int count = 0;
+
         if (n != k) {
             return false;
         }
 
         char a[] = new char[n];
         char b[] = new char[k];
-
-        // Copy strings into arrays
         for (int i = 0; i < n; i++) {
             a[i] = s.charAt(i);
             b[i] = goal.charAt(i);
         }
 
-        int count = 0;
-        int first = -1;
-        int second = -1;
-
-        // Find different positions
+        // Find differences
         for (int i = 0; i < n; i++) {
 
             if (a[i] != b[i]) {
-
                 count++;
-
-                if (count == 1) {
-                    first = i;
-                }
-
-                if (count == 2) {
-                    second = i;
-                }
             }
         }
 
@@ -47,13 +34,29 @@ class Solution {
         // Exactly 2 differences
         if (count == 2) {
 
-            // Swap the two characters in b
+            int first = -1;
+            int second = -1;
+
+            for (int i = 0; i < n; i++) {
+
+                if (a[i] != b[i]) {
+
+                    if (first == -1) {
+                        first = i;
+                    } else {
+                        second = i;
+                    }
+                }
+            }
+
+            // Swap
             char temp = b[first];
             b[first] = b[second];
             b[second] = temp;
 
-            // Compare after swapping
+            // Check if equal
             for (int i = 0; i < n; i++) {
+
                 if (a[i] != b[i]) {
                     return false;
                 }
@@ -61,12 +64,10 @@ class Solution {
 
             return true;
         }
-
-        // No differences
-        // Need at least one duplicate character
         if (count == 0) {
 
             for (int i = 0; i < n; i++) {
+
                 for (int j = i + 1; j < n; j++) {
 
                     if (a[i] == a[j]) {
@@ -74,8 +75,9 @@ class Solution {
                     }
                 }
             }
-        }
 
+            return false;
+        }
         return false;
     }
 }
