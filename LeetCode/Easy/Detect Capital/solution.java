1b0c395bcf3c0645088b27f7f36763a1e6b680ea
mohammadarshalcs28 <mohamma.arshal.cs28@iilm.edu>
2026-09-01 10:03:08 +0530

    Solved: Detect Capital [Easy]

---
+++ LeetCode/Easy/Detect Capital/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count=0;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if(Character.isUpperCase(c)==true){
+                count++;
+            }
+        }
+        if(count==0){
+            return true;
+        } 
+        if(count==word.length()){ 
+            return true;
+        }
+        if (count == 1 && Character.isUpperCase(word.charAt(0))) {
+            return true;
+        }
+        else return false;
+    }
+}
